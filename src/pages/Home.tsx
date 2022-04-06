87a80c4fbfae1752ad4206096e8824ecf2d7525c
gustavo-sobbrero-develcode <gustavo.sobbrero@develcode.com.br>
2022-04-06 14:26:40 -0300

    Primeiro commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,95 @@
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text, TextInput, FlatList} from 'react-native';
+import {Button} from '../components/Button';
+import {SkillCard} from '../components/SkillCard';
+
+interface SkillData {
+  id: string;
+  name: string;
+}
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState(''); // Armazena nova skill
+  const [mySkills, setMySkills] = useState<SkillData[]>([]); // Armazena minhas skills
+  const [gretting, setGretting] = useState('');
+
+  function handleAddNewSkill() {
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    };
+    setMySkills(oldState => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(skill => skill.id !== id));
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour > 6 && currentHour < 12) {
+      setGretting('Good Morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGretting('Good Afternoon');
+    } else {
+      setGretting('Good Nigth');
+    }
+  }, []);
+
+  return (
+    // Aqui ficam os elementos visuais
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Gustavo</Text>
+
+      <Text style={styles.grettings}>{gretting}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+
+      <Button onPress={handleAddNewSkill} title="Add" />
+
+      <Text style={[styles.title, {marginVertical: 50}]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#121015',
+    flex: 1,
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: '#fff',
+    padding: 15,
+    marginTop: 25,
+    borderRadius: 8,
+    fontSize: 18,
+  },
+  grettings: {
+    color: '#FFF',
+  },
+});
